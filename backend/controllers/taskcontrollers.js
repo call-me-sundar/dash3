e59307ae262b8c6a38f88fd1d556ee6a05385ab4
Sundar <sundar10072001@gmail.com>
2023-09-11 17:52:03 +0530

    'new'

--- backend/controllers/taskcontrollers.js
+++ backend/controllers/taskcontrollers.js
@@ -9,7 +9,7 @@ const getAllData = async (req, res) => {
     }
 }
 
-const columnDetails = async (req, res) => {
+const columnDetails = async (req, res) => { 
     try {
         const [result] = await db.query("select * from columndetails");
         res.status(200).json(result);
@@ -24,7 +24,7 @@ const updatedTableCheckedDatas = async (req, res) => {
     try {
         const [result] = await db.query(
             "UPDATE columndetails SET checked = ? WHERE column_name = ?;",
-            [column_value , column_name]
+            [column_value, column_name]
         );
 
         res.status(200).json(result);
@@ -33,4 +33,24 @@ const updatedTableCheckedDatas = async (req, res) => {
     }
 };
 
-module.exports = { getAllData, columnDetails, updatedTableCheckedDatas }
+const updatedColumnName = async (req, res) => {
+    const { changedColumnName, columnName, column_datatype } = req.body; // Destructure the body object
+    try {
+        // Update the column name in the database table 
+        const updateQuery = "UPDATE columndetails SET column_name = ? WHERE column_name = ?";
+        const [updateResult] = await db.query(updateQuery, [changedColumnName, columnName]);
+        
+        // Change the column name in the dashboard table 
+        const alterQuery = `ALTER TABLE dashboardtable CHANGE COLUMN ${columnName} ${changedColumnName} ${column_datatype}`;
+        await db.query(alterQuery);
+        res.status(200).json(updateResult);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+        // Corrected JSON response format and included error message    
+    }
+};
+
+
+
+
+module.exports = { getAllData, columnDetails, updatedTableCheckedDatas, updatedColumnName }
--- backend/routes/taskroutes.js
+++ backend/routes/taskroutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const route = express.Router();
 
-const {getAllData, columnDetails, updatedTableCheckedDatas} = require('../controllers/taskcontrollers')
+const {getAllData, columnDetails, updatedTableCheckedDatas, updatedColumnName} = require('../controllers/taskcontrollers')
 
 route.get('/' , getAllData);
 route.get('/columndetails' , columnDetails);
 route.patch('/updatedTableCheckedDatas' , updatedTableCheckedDatas);
+route.put('/updatedColumnName' , updatedColumnName);
 
 module.exports = route;
